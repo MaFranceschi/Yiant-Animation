ed5632bb10fae20dc0631b885ec5dda70194e777
Marco Franceschi <marcofranceschi@hotmail.com>
2014-12-23 02:32:15 -0430

    Cambios varios

---
+++ public/canvas.js
@@ -0,0 +1,37 @@
+(function(){
+	var canvas = document.getElementsByTagName('canvas');
+	var context = canvas[0].getContext("2d");
+
+//	context.moveTo(0,0);
+//	context.lineTo(1000,500);
+// Canvas 200 height, 300 width
+//	context.stroke();
+//Y izquierda
+	context.beginPath();
+	context.moveTo(150,150);
+	context.lineTo(135,150);
+	context.lineTo(135,120);
+	context.lineTo(115,100);
+	context.lineTo(130,90);
+	context.lineTo(150,115);
+	context.lineTo(150,150);
+	context.stroke();
+
+	context.beginPath();
+	context.moveTo(155,150);
+	context.lineTo(170,150);
+	context.lineTo(170,120);
+	context.lineTo(195,100);
+	context.lineTo(180,100);
+	context.lineTo(165,110);
+	context.lineTo(140,75);
+	context.lineTo(140,90);
+	context.lineTo(155,115);
+	context.lineTo(155,150);
+//	context.lineTo(90,15);
+
+	context.stroke();
+
+
+
+})()
